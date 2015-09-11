4fbb54eb4557151a89117b709ddb6721dac13152
mayuri <mayuribangar89@gmail.com>
2015-09-11 13:49:53 +0530

    this is old file
    updated old file

--- calci.cpp
+++ calci.cpp
@@ -2,3 +2,4 @@ Hello,
     I am mayuri from cummins college of engineering.
     This is ESDL session.
     ESDL stands for employibility skills developement lab.
+we are creating offline repository today.
